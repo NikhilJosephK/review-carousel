214657ca4d00bf6eac4ab72a40a10829f1fa97b4
nikhil joseph <nikhilnjk@gmail.com>
2023-09-24 16:56:25 +0530

    functionality of the app slider or carousel is done, styling remaining

---
+++ index.js
@@ -0,0 +1,97 @@
+let array = [
+  {
+    id: 1,
+    name: "john",
+    age: "30",
+    job: "IT",
+  },
+  {
+    id: 2,
+    name: "jacob",
+    age: "35",
+    job: "railways",
+  },
+  {
+    id: 3,
+    name: "jason",
+    age: "40",
+    job: "pilot",
+  },
+  {
+    id: 4,
+    name: "rebecca",
+    age: "34",
+    job: "content creator",
+  },
+];
+
+let number = 0;
+
+window.addEventListener('load',function(){
+
+    if(number === 0){
+        number=1;
+    }
+
+    render([array[0]])
+})
+
+let btnIncrease = document.querySelector(".btn-increase");
+let btnDecrease = document.querySelector(".btn-decrease");
+
+
+
+
+// increase function or move slider to the right
+
+btnIncrease.addEventListener("click", function () {
+  number += 1;
+
+  if (number === 5) {
+    number = 1;
+  }
+  console.log(number);
+
+  let information = array.filter((item) => {
+    return number.toString().includes(item.id.toString());
+  });
+
+  render(information);
+});
+
+// decrease function or move slider to the left
+
+btnDecrease.addEventListener("click", function () {
+    number -= 1;
+  if (number === -1 || number === 0) {
+    number = 4;
+  }
+
+  console.log(number);
+
+  let information = array.filter((item) => {
+    return number.toString().includes(item.id.toString());
+  });
+
+  render(information);
+});
+
+
+// render function 
+
+function render(data){
+
+ let container = document.querySelector('.container');
+ container.innerHTML = "";
+
+    data.map(item=>{
+       container.innerHTML = `
+       <div class="wrapper">
+       <h3 class="name">${item.name}</h3>
+       <h4 class="age">${item.age}</h4>
+       <h5 class="job">${item.job}</h5>    
+        </div>
+       `
+    })
+
+}
